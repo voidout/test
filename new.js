76ec4f38c17c60644395714be93cfaba7abb623c
voidout <65542456+voidout@users.noreply.github.com>
2022-04-08 10:12:40 +0530

    Update new.js

--- new.js
+++ new.js
@@ -2,7 +2,5 @@
 
 let textBox = document.getElementById('good');
 textBox.addEventListener('keydown', (event) => {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
         textBox.innerHTML += event.key.toLowerCase();
-    }
-}, true);
+});
